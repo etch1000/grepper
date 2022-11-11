e0d8e3ef72a3251f30f6a08c6efe2f870d135bc7
etch1000 <hmoroliya@ariqt.com>
2022-11-11 12:19:14 +0530

    Improved Error Message

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,14 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_arguments(&args);
+    let config = Config::new(&args);
 
     println!("Searching for : {}", config.query);
 
     println!("In file : {}", config.file_path);
-    
-    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text : \n{contents}");
 }
@@ -20,10 +21,15 @@ struct Config {
     file_path: String,
 }
 
-fn parse_arguments(args: &[String]) -> Config {
-    let query = args[1].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("Not enough arguments");
+        }
+        let query = args[1].clone();
 
-    let file_path = args[2].clone();
+        let file_path = args[2].clone();
 
-    Config { query, file_path }
+        Config { query, file_path }
+    }
 }
