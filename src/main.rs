b93e1f944be6601dc70482e3477e2bd82f150897
etch1000 <hmoroliya@ariqt.com>
2022-11-14 12:24:30 +0530

    Code spitted into a library crate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
+use grepper::Config;
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
 
 fn main() {
@@ -15,35 +14,9 @@ fn main() {
 
     println!("In file : {}", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = grepper::run(config) {
         println!("Application Error: {e}");
         process::exit(1);
     }
 }
 
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-
-        let query = args[1].clone();
-
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
