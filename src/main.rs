c7850267e1d25a5084e72f1a07911504c99f8876
etch1000 <hmoroliya@ariqt.com>
2022-11-14 11:49:52 +0530

    Extracted logic from main and return Result from run function and handling it in main using if let

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -14,10 +15,10 @@ fn main() {
 
     println!("In file : {}", config.file_path);
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    println!("With text : \n{contents}");
+    if let Err(e) = run(config) {
+        println!("Application Error: {e}");
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -30,10 +31,19 @@ impl Config {
         if args.len() < 3 {
             return Err("Not enough arguments");
         }
+
         let query = args[1].clone();
 
         let file_path = args[2].clone();
 
         Ok(Config { query, file_path })
     }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
 }
